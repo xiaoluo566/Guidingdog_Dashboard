f428df6f337f7bbccd8bc5a67670c9803046f5aa
Nixiak <2269387402@qq.com>
2025-11-28 14:40:54 +0800

    second commit

---
+++ Guidingdog_Dashboard/src/main/java/com/nixiak/guidingdog_dashboard/GuidingdogDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.nixiak.guidingdog_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidingdogDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuidingdogDashboardApplication.class, args);
+    }
+
+}
